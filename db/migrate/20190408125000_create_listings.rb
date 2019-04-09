809dd34647c5a9a8ec850ed2893fb21733132c69
masha-b <mashabudryte@gmail.com>
2019-04-09 13:17:44 +0100

    create_listing migration updated

--- db/migrate/20190408125000_create_listings.rb
+++ db/migrate/20190408125000_create_listings.rb
@@ -3,13 +3,14 @@ class CreateListings < ActiveRecord::Migration[5.2]
     create_table :listings do |t|
       t.string :name
       t.string :location
-      t.string :city
+      t.datetime :start_date
+      t.datetime :end_date
 
       t.float :price_per_night
       # add_foreign_key :user_id
       t.timestamps null: false
       t.references :user, foreign_key: true
 
-    end 
+    end
   end
 end
