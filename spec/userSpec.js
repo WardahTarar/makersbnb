66b6d2c46e58a6f24e7a5205f1b2dfe1a6e1d71c
Cosmin <mirceacosmin@gmail.com>
2019-04-09 10:29:00 +0100

    #Updated gitignore

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,24 +1,18 @@
-
-describe('user js file', function(){
-
+describe("user js file", function() {
   var user;
 
-  beforeEach(function(){
+  beforeEach(function() {
     user = new User();
-  })
-
-  it('creates a user object', function(){
-
-    expect(user.createUser(
-      'James',
-      'Davies',
-      'test@rubbish.com',
-      '12345')).toEqual({
-        firstName: 'James',
-        lastName: 'Davies',
-        email: 'test@rubbish.com',
-        password: '12345'
-      }) 
   });
 
+  it("creates a user object", function() {
+    expect(
+      user.createUser("James", "Davies", "test@rubbish.com", "12345")
+    ).toEqual({
+      firstName: "James",
+      lastName: "Davies",
+      email: "test@rubbish.com",
+      password: "12345"
+    });
+  });
 });
