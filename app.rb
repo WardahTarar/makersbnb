e3b891c9d4893f0f869e06d46b1275c1074a0044
saypop <tome.m.dj@gmail.com>
2019-04-09 09:34:46 +0100

    Unit tests passing for request objects and prototypes.

--- app.rb
+++ app.rb
@@ -3,16 +3,12 @@ current_dir = Dir.pwd
 
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
-
 class Makersbnb < Sinatra::Base;
 
-
-
-
-
-
+get '/spaces/:space_id' do
+  erb :spaces
+end
 
 run! if app_file == $0
 
-
-end 
+end
