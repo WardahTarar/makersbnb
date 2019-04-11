c7df0519f0b97edf13547ecc6d1bb8faf424dec7
alexanderchen6 <alexbaggio315@hotmail.com>
2019-04-11 18:03:05 +0100

    testing

--- spec/features/addListingSpec.js
+++ spec/features/addListingSpec.js
@@ -1,13 +1,7 @@
-// var popupCommand = {
-//   popupCheck:function(){
-//  return this.waitForElementVisible('submitCreate', 5000)
-//    .click('submitCreate', isPresent)
-//    .click('submitCreate', isPresent)
-//   }
-
 module.exports = {
   "User can sign up": function(browser) {
     browser
+    // signup
       .url("localhost:9292/")
       .click("#signup")  
       .waitForElementPresent('#modalRegisterForm',20000, 'Some message here to show while running test')
@@ -21,9 +15,15 @@ module.exports = {
       .setValue("input[name=password]", "12345")
       .click("#submitCreate")
 
-      .waitForElementVisible("#personalMess")
-      .assert.containsText("#personalMess", "Welcome John")
-      .end();
-      
+      .waitForElementPresent('#listing',20000, 'Some message here to show while running test')
+      .click("#listing")
+      .setValue("input[name=firstName]", "John")
+      .setValue("input[name=lastName]", "Snow")
+      .setValue("input[name=email]", "jonSnow@gmail.com")
+      .setValue("input[name=password]", "12345")
+
+
+
+
   }
 };
